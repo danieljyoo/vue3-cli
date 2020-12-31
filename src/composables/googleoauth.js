bc9c796c17db1500d8aa6252b33f4212e859f99d
Daniel Jhin Yoo <danieljyoo@gmail.com>
2020-12-31 12:28:55 -0800

    initial generated code from vue-cli

---
+++ src/composables/googleoauth.js
@@ -0,0 +1,18 @@
+
+import { ref, computed } from 'vue'
+
+export default function useGoogleOAuth() {
+  const searchQuery = ref('')
+  const repositoriesMatchingSearchQuery = computed(() => {
+    return repositories.value.filter(repository => {
+      return repository.name.includes(searchQuery.value)
+    })
+  })
+
+  return {
+    searchQuery,
+    repositoriesMatchingSearchQuery
+  }
+}
+
+
